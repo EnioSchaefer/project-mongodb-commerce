a41b577da090e4f556921d690198883a0575de50
Enio Schaefer <enio100schaefer@hotmail.com>
2023-04-17 20:10:38 -0300

    feat: diminui tamanho de query

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,12 +1,12 @@
 db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
 
 db.produtos.updateMany(
-  { tags: { $elemMatch: { $eq: "bovino" } } },
+  { tags: { $in: ["bovino"] } },
   { $inc: { avaliacao: 5 } }
 );
 
 db.produtos.updateMany(
-  { tags: { $elemMatch: { $eq: "ave" } } },
+  { tags: { $in: ["ave"] } },
   { $inc: { avaliacao: 3 } }
 );
 
